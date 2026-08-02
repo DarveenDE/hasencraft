00dceafa41303544eb569c4e0fdd4d5f34753d8a
Hasencraft <hasencraft@local.invalid>
2026-08-02 19:16:45 +0200

    Make cooked eggs interchangeable

--- kubejs/server_scripts/hasencraft.js
+++ kubejs/server_scripts/hasencraft.js
@@ -1,1 +1,10 @@
-// Reserved for Hasencraft server customizations.
+// Keep equivalent cooked eggs interchangeable across mods.
+ServerEvents.tags('item', event => {
+  const cookedEggs = [
+    'farmersdelight:fried_egg',
+    'naturalist:cooked_egg'
+  ]
+
+  event.add('c:foods/cooked_egg', cookedEggs)
+  event.add('c:cooked_eggs', cookedEggs)
+})
